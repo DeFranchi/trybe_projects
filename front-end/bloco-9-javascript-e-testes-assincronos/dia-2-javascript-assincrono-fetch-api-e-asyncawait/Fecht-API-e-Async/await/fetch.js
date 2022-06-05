566d1b637bdfa0d7d747a52624c1ee3680073f8d
Debora Franchi <deborafranchicarvalho@gmail.com>
2022-06-05 12:21:08 +0200

    praticando dia 2

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/Fecht-API-e-Async/await/fetch.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/Fecht-API-e-Async/await/fetch.js
@@ -17,9 +17,9 @@ const fetch = require('node-fetch');
 const fetchJoke = () => {
   const url = 'api.chucknorris.io/jokes/random?category=dev';
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data.value))
+  fetch(url) // promise 
+    .then((response) => response.json()) //converteu para json
+    .then((data) => console.log(data.value)) //pegar os values do obj
     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
 }
 
